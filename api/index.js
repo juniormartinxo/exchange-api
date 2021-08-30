6e51ebeb774832f1f10db3b0620b00b00051ef91
Junior Martins <amjr.box@gmail.com>
2021-08-29 22:02:42 -0300

    Transformando a função em assíncrona

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ const url = 'https://exchangenow.net/ajax-fn?function=multiploMoeda&p1=2'
 const url2 =
   'https://exchangenow.net/ajax-fn?function=taxas_remessas&p1=2&p2=10000&p3=0&p4=5.1970'
 
-function buscaDadosExchangeNow(url) {
-  fetch(url)
+async function buscaDadosExchangeNow(url) {
+  await fetch(url)
     .then(response => {
       response.json().then(data => {
         console.log(data)
