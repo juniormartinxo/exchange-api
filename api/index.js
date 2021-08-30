cb52e842f0f47fab65e531ab37dd9646624053ac
Junior Martins <amjr.box@gmail.com>
2021-08-29 21:59:40 -0300

    Create function

--- api/index.js
+++ api/index.js
@@ -2,12 +2,20 @@ const fetch = require('node-fetch')
 
 const url = 'https://exchangenow.net/ajax-fn?function=multiploMoeda&p1=2'
 
-fetch(url)
-  .then(response => {
-    response.json().then(data => {
-      console.log(data)
+const url2 =
+  'https://exchangenow.net/ajax-fn?function=taxas_remessas&p1=2&p2=10000&p3=0&p4=5.1970'
+
+function buscaDadosExchangeNow(url) {
+  fetch(url)
+    .then(response => {
+      response.json().then(data => {
+        console.log(data)
+      })
     })
-  })
-  .catch(error => {
-    console.log(error)
-  })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+buscaDadosExchangeNow(url)
+buscaDadosExchangeNow(url2)
