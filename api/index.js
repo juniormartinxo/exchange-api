2a1a415a24326b6827e44f10c3faec3168c65dc2
Junior Martins <amjr.box@gmail.com>
2021-08-29 21:52:33 -0300

    First commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const url = 'https://exchangenow.net/ajax-fn?function=multiploMoeda&p1=2'
+
+fetch(url)
+  .then(response => {
+    response.json().then(data => {
+      console.log(data)
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
